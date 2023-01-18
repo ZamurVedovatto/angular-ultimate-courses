a219a2aadb90f1f98d042bd1db7caf6c93837cea
Zamur Borges Vedovatto <vedovatto@gmail.com>
2023-01-18 17:11:20 -0300

    styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,31 @@ import { Component } from "@angular/core";
   selector: "app-root",
   template: `
     <div class="app">
+      <header>
+        <img src="/assets/img/logo.svg" alt="Logo" class="logo" />
+      </header>
       <donut-list></donut-list>
     </div>
   `,
   styles: [
     `
+      header {
+        display: flex;
+        justify-content: center;
+        margin-bottom: 25px;
+      }
+      .logo {
+        width: 100px;
+        height: 88px;
+      }
       .app {
-        margin-top: 50px;
-        font-size: 22px;
-        color: #fff;
-        text-align: center;
+        background-color: #f1f1f1;
+        border-radius: 8px;
+        max-width: 400px;
+        width: 94%;
+        margin: 25px auto;
+        padding: 25px;
+        border: 4px solid #ef9fc7;
       }
     `,
   ],
