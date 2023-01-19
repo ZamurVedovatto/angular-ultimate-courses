9e848daa22253d7ee1bc856b592ba3fd130ff227
Zamur Borges Vedovatto <vedovatto@gmail.com>
2023-01-18 21:18:46 -0300

    viewencapsulation - shadowdom or emulated (default)

--- src/app/admin/components/donut-card/donut-card.component.ts
+++ src/app/admin/components/donut-card/donut-card.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, ViewEncapsulation } from "@angular/core";
 
 import { Donut } from "./../../models/donut.model";
 
 @Component({
   selector: "donut-card",
+  encapsulation: ViewEncapsulation.Emulated,
   templateUrl: "./donut-card.component.html",
   styleUrls: ["./donut-card.component.scss"],
 })
